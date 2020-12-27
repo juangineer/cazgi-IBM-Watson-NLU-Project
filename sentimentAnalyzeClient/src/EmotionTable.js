9cad2290e597779bf30ad14c9b6bca67be406296
juangineer <juangineer@gmail.com>
2020-12-27 12:20:06 -0500

    Added emotion scores

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,15 +3,16 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
+      var emotions = this.props.emotions;
       return (  
         <div>
           <table className="table table-bordered">
             <tbody>
             {
-            Object.keys(this.props.emotions).map((e,i) => {
+            Object.keys(emotions).map((e,i) => {
                     return <tr>
                       <td>{e}</td>
-                      <td>{i}</td>
+                      <td>{emotions[e]}</td>
                     </tr>;
             })}
             </tbody>
